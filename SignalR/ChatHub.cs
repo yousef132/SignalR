5a8021542c873649aed3e6d5cd90c519ce7045d8
Yousef Saad <saady8454@gmail.com>
2024-08-12 03:35:24 +0300

    Add project files.

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string name, string message)
+        {
+            Clients.Others.newMessage(name, message);
+        }
+
+    }
+}
---
+++ SignalR/Startup1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalR.Startup1))]
+
+namespace SignalR
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
