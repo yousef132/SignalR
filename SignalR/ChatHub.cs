8b6067378dba830cbd7bcc00a5251d97645df5c5
Yousef Saad <saady8454@gmail.com>
2024-08-13 03:23:08 +0300

    Add Join Group And Send Message To Group

--- SignalR/ChatHub.cs
+++ SignalR/ChatHub.cs
@@ -6,9 +6,27 @@ namespace SignalR
     [HubName("chatHub")]
     public class ChatHub : Hub
     {
-        public void sendMessage(string name, string message)
+        public void sendMessage(string name, string inputMessage)
         {
-            Clients.Others.newMessage(name, message);
+
+
+            // save in db 
+            // do some logic 
+            Clients.Others.newMessage(name, inputMessage);
+        }
+
+        public void joinGroup(string groupName, string clientName)
+        {
+            // add client to group
+            Groups.Add(Context.ConnectionId, groupName);
+
+            // invoke the event to all other client in the same group
+            Clients.OthersInGroup(groupName).joinGroup(clientName, groupName);
+        }
+
+        public void sendGroup(string groupName, string clientName, string message)
+        {
+            Clients.Group(groupName).sendGroup(clientName, message);
         }
 
     }
